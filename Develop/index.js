f24c60e42a2781d7142e60512ed18e974e76e884
PRamsey02 <prestonkramsey02@icloud.com>
2022-10-21 21:22:28 -0500

    Added README prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,62 @@
 // TODO: Include packages needed for this application
+const  inquirer = require('inquirer');
+const fs = require('fs');
+const generateReadme = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Project name: ',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Description of your project: ',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Provide instructions and examples for use. Include screenshots as needed.',
+      name: 'usage',
+    },
+    {
+      type: 'list',
+      message: 'Choose your license.',
+      name: 'license',
+      choices: [
+        {value: 'Apache 2.0'},
+        {value: 'BSD 3-Clause'},
+        {value: 'GNU LGPL v3'},
+        {value: 'MIT'},
+        {value: 'Mozilla Public License 2.0'},
+        {value: 'None'},
+      ]
+    },
+    {
+      type: 'input',
+      message: 'Who are the contributors for this project?',
+      name: 'contributors'
+    },
+    {
+      type: 'input',
+      message: 'Provide tests for project, and explain how to test.',
+      name: 'test'
+    },
+    {
+      type: "input",
+      message: "Enter your GitHub username: ",
+      name: "username",
+    },
+    {
+      type: "input",
+      message: "Enter your email: ",
+      name: "email"
+    }
+  ])
 
 // TODO: Create a function to initialize app
 function init() {}
